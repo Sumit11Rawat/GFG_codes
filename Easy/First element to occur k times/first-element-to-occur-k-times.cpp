f3e1dcea5e6ee2df9d6698bd447e2e17f7b7c145
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-03-02 05:10:31 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/First element to occur k times/first-element-to-occur-k-times.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int firstElementKTime(int n, int k, int a[])
+    {
+        int ans=-1;
+        unordered_map<int,int>m;
+        for(int i=0;i<n;i++){
+            m[a[i]]++;
+            if(m[a[i]]==k){
+                ans=a[i];
+                break;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n, k;
+	    cin >> n >> k;
+	    int a[n];
+	    for(int i = 0;i<n;i++){
+	        cin >> a[i];
+	    }
+	    Solution ob;
+	    cout<<ob.firstElementKTime(n, k, a)<<endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
